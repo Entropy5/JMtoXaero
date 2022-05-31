171ef82891dea7b509d0eb0ecfaa84a5e812f342
lamp <virtual@riseup.net>
2022-05-31 22:56:25 +0200

    merge entropy changes manually

--- src/main/java/com/github/entropy5/JourneyMapToXaero.java
+++ src/main/java/com/github/entropy5/JourneyMapToXaero.java
@@ -1,12 +1,13 @@
 package com.github.entropy5;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collections;
-import java.util.Map;
+import java.util.*;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -38,26 +39,92 @@ public class JourneyMapToXaero {
         Path folderIn = new File(String.format("%s/DIM%d/%s", input, dimension, slice)).toPath();
         Path folderOut = new File(String.format("%s/%s/mw$default/", output, (dimension == 0 ? "null" : "DIM" + dimension))).toPath();
 
+        processDimension(folderIn, folderOut, dimension == -1);
+    }
+
+    private static void processDimension(Path folderIn, Path folderOut, boolean nether) {
+        File folderCheck = new File(String.valueOf(folderOut.toFile()));
+        File parentCheck = new File(String.valueOf(folderOut.toFile().getParentFile()));
+        if (!parentCheck.exists()){
+            parentCheck.mkdir();
+            folderCheck.mkdir();
+        } else if (!folderCheck.exists()) {
+            folderCheck.mkdir();
+        }
+        List<String> caveLayers = new ArrayList<>();
+        for (int i = 0; i < 16; i++) caveLayers.add(i + "");
+
+        if (nether) {
+            HashMap<String, HashSet<File>> allCaveFiles = new HashMap<>();   // region file -> all cave file locations
+            for (File folder : Objects.requireNonNull(folderIn.toFile().listFiles())) {
+                for (File file : Objects.requireNonNull(folder.listFiles())) {
+                    String layer = file.getParentFile().getName();  // 0, 1, 2, day, night
+                    String region = file.getName();  // 1,1.png
+                    if (caveLayers.contains(layer)) {  // if this folder is not day, night, or something weird
+                        HashSet<File> regionFiles;
+                        if (allCaveFiles.containsKey(region)) {
+                            regionFiles = allCaveFiles.get(region);
+                        } else {
+                            regionFiles = new HashSet<>();
+                        }
+                        regionFiles.add(file);
+                        allCaveFiles.put(region, regionFiles);
+                    }
+                }
+            }
+            for (String region : allCaveFiles.keySet()) {
+                List<File> fileList = new ArrayList<>(allCaveFiles.get(region));
+                Collections.sort(fileList);
+
+                BufferedImage combined = new BufferedImage(512, 512, BufferedImage.TYPE_INT_ARGB);
+                Graphics tempGraphics = combined.getGraphics();
+                try {
+                    for (File image : fileList) {
+                        BufferedImage temp = ImageIO.read(image);
+                        tempGraphics.drawImage(temp, 0, 0, null);
+                    }
+                    String[] parts = region.split("[.,]");
+                    processRegion(true, folderOut, combined, parts, fileList.get(0));
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return;
+        }
+
+        // Overworld and End
         File[] files = folderIn.toFile().listFiles();
-        assert files != null;
+        if (files == null) {
+            return;
+        }
         for (File file : files) {
             if (file.isFile()) {
                 String[] parts = file.getName().split("[.,]");
-                if (parts.length == 3 && parts[2].equals("png")) {
-                    //TODO: this should be a thread worker instead. otherwise it will be very laggy (similar thing to mc multiplayer or tab)
-                    try {
-                        int rx = Integer.parseInt(parts[0]);
-                        int rz = Integer.parseInt(parts[1]);
-                        String zipName = rx + "_" + rz + ".zip";
-                        File zipFile = folderOut.resolve(zipName).toFile();
-                        BufferedImage image = ImageIO.read(file);  // JourneyMap image IN
-                        new JourneyMapToXaero().saveRegion(image, zipFile);
-                        System.out.println("Converted " + file.toString().split("journeymap")[1] + " to " + zipFile.toString().split("XaeroWorldMap")[1]);
-
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+                BufferedImage image;
+                try {
+                    image = ImageIO.read(file);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
+                processRegion(false, folderOut, image, parts, file);
+            }
+        }
+    }
+
+    private static void processRegion(boolean nether, Path dim_path_out, BufferedImage file, String[] parts, File location) {
+        if (parts.length == 3 && parts[2].equals("png")) {
+            //TODO: this should be a thread worker instead. otherwise it will be very laggy (similar thing to mc multiplayer or tab)
+            try {
+                int rx = Integer.parseInt(parts[0]);
+                int rz = Integer.parseInt(parts[1]);
+                String zipName = rx + "_" + rz + ".zip";
+                File zipFile = dim_path_out.resolve(zipName).toFile();
+                new JourneyMapToXaero().saveRegion(file, zipFile, nether);
+                System.out.println("Converted " + location.toString().split("journeymap")[1] + " to " + zipFile.toString().split("XaeroWorldMap")[1]);
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
@@ -67,11 +134,13 @@ public class JourneyMapToXaero {
 
         private final int journeymapColor;
 
-        private final boolean caveBlock = false;
+        private final boolean nether;
+
         private final int colourType = 3; // 3 for no complexity
 
-        public IronBlock(final int jmColor) {
+        public IronBlock(final int jmColor, boolean nether) {
             this.journeymapColor = jmColor;
+            this.nether = nether;
         }
 
 
@@ -79,10 +148,11 @@ public class JourneyMapToXaero {
             final int colourTypeToWrite = this.colourType & 3;
             int height = 64;
             int parameters = (0);
+            int light = this.nether ? 15 : 0;
             parameters |= this.isNotGrass() ? 1 : 0;
             parameters |= this.getNumberOfOverlays() != 0 ? 2 : 0;
             parameters |= colourTypeToWrite << 2;
-            parameters |= this.caveBlock ? 128 : 0;
+            parameters |= light << 8;
             parameters |= height << 12;
             parameters |= 1048576;
             // ignoring some properties here
@@ -119,7 +189,7 @@ public class JourneyMapToXaero {
     }
 
 
-    public void saveRegion(final BufferedImage image, File zipFile) {
+    public void saveRegion(final BufferedImage image, File zipFile, boolean nether) {
         try {
 
             DataOutputStream out = null;
@@ -146,7 +216,7 @@ public class JourneyMapToXaero {
                                     for (int z = 0; z < 16; ++z) {
                                         int relX = 64 * o + 16 * i + x;
                                         int relZ = 64 * p + 16 * j + z;
-                                        this.savePixel(new IronBlock(image.getRGB(relX, relZ)), out);
+                                        this.savePixel(new IronBlock(image.getRGB(relX, relZ), nether), out);
                                     }
                                 }
                                 out.write(0); // some version thing
