9c28b2547bb3bad3b2ca10a7cc45188928698451
Entropy5 <44469915+Entropy5@users.noreply.github.com>
2022-06-03 02:08:04 +0200

    Changes by IronException

--- src/main/java/com/github/entropy5/JourneyMapToXaero.java
+++ src/main/java/com/github/entropy5/JourneyMapToXaero.java
@@ -20,17 +20,24 @@ import java.util.zip.ZipOutputStream;
 
 
 public class JourneyMapToXaero {
-    public static HashMap<Integer,Integer> mapping = readMapping();
+    public static final HashMap<Integer,Integer> COLOR_TO_STATE = readMapping(); // THIS HAS TO BE color -> state. switch the loading if need to
+
+    private static int out = 0;
 
     public static void main(final String[] args) {
-        if (args.length < 2) {
+        if (args.length < 1 ||args.length == 2) {
             System.err.println("usage: <input folder> <output folder> <dimension id>");
+            System.out.println("or just do: <output folder> (it will only generate all the possible Xaro colors tho that you can do with state and colorTypes)");
             System.exit(1);
         }
 
         String input = args[0];
-        String output = args[1];
+        if(args.length < 2) {
+            generate(input); // its actually output but whatever
+            return;
+        }
 
+        String output = args[1];
         if (!(args[2].equals("all"))) {
             int dimension = Integer.parseInt(args[2]);
             processDimension(input, output, dimension);
@@ -39,6 +46,110 @@ public class JourneyMapToXaero {
                 processDimension(input, output, i);
             }
         }
+    }
+
+    public static void generate(final String output) {
+        Path folderOut = new File(String.format("%s/%s/mw$default/", output, "null" )).toPath();
+
+        File parentCheck = new File(String.valueOf(folderOut.toFile().getParentFile()));
+        if (!parentCheck.exists()) {
+            parentCheck.mkdir();
+        }
+        for (int regionX = 0; regionX < 50000 >> 9; regionX++) { // TODO maybe increase the steps?
+            for (int regionZ = 0; regionZ < 1; regionZ++) { // color codes...
+                doRegion(folderOut, regionX, regionZ);
+            }
+        }
+
+
+    }
+
+    private static void doRegion(final Path where, final int rx, final int rz) {
+        try {
+
+            DataOutputStream out = null;
+
+            try {
+                final ZipOutputStream zipOut = new ZipOutputStream(new BufferedOutputStream(Files.newOutputStream(where.resolve(rx + "_" + rz + ".zip"))));
+                out = new DataOutputStream(zipOut);
+                final ZipEntry e = new ZipEntry("region.xaero");
+                zipOut.putNextEntry(e);
+                out.write(255);  // mimicking logic from Xaero format
+                out.writeInt(4);
+                int o = 0;
+
+                while (true) {
+                    if (o >= 8) {  // A Region consists of 8 x 8 TileChunks, each size 64
+                        zipOut.closeEntry();
+                        break;
+                    }
+                    for (int p = 0; p < 8; ++p) {
+                        out.write(o << 4 | p);
+                        for (int i = 0; i < 4; ++i) {
+                            for (int j = 0; j < 4; ++j) {
+                                final IronBlock block = getIronBlockInRegionAt((512 * rx + 64 * o + 16* i) / 16 - 1, (512 * rz + 64 * p + 16 * j) / 16);
+                                for (int x = 0; x < 16; ++x) {
+                                    for (int z = 0; z < 16; ++z) {
+                                        savePixel(block, out);
+                                    }
+                                }
+                                out.write(0); // some version thing
+                            }
+                        }
+                    }
+                    ++o;
+                }
+            } finally {
+                if (out != null) {
+                    out.close();
+                }
+            }
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static IronBlock getIronBlockInRegionAt(final int x, final int z) {
+        /* int colorType = (z % 7) ;
+         int color = 0;
+         if(colorType > 3) {
+             switch (colorType - 3) {
+                 case 0:
+                     color = 0;
+                     break;
+                 case 1:
+                     color = 0x00010101;
+                     break;
+                 case 2:
+                     color = 0x00FFFFFF;
+                     break;
+                 case 3:
+                     color = 0xFF000000;
+                     break;
+                 case 4:
+                     color = 0xFF010101;
+                     break;
+                 case 5:
+                     color = 0xFFFFFFFF;
+                     break;
+             }
+             colorType = 3;
+
+         }**/
+        if(x < 0) {
+            return new IronBlock(2, 0, 0, z) {
+                @Override
+                public int getParameters() { // makes it think there is no grass but there actually is...
+                    int parameters = 0;
+                    parameters |= 1;
+                    parameters |= 64 << 12;
+                    parameters |= 1048576;
+                    // ignoring some properties here
+                    return parameters;
+                }
+            };
+        }
+        return new IronBlock(x + 1, 0, 0, z);
     }
 
     public static HashMap<Integer,Integer> readMapping() {
@@ -65,8 +176,11 @@ public class JourneyMapToXaero {
         return mapping;
     }
 
-    public static int getClosestColorBlock(int color) {
-        return mapping.entrySet().stream().min(Comparator.comparing(i -> calcDistance(i.getKey(), color >> 16 & 255, color >> 8 & 255, color & 255))).map(Map.Entry::getValue).orElse(0);
+
+    public static int getClosestColor(int desiredColor) {
+        int newColor = COLOR_TO_STATE.keySet().stream().min(Comparator.comparing(i -> calcDistance(i, desiredColor >> 16 & 255, desiredColor >> 8 & 255, desiredColor & 255))).orElse(0);
+//        System.out.println("-Processed " + new Color(desiredColor) + " into " + new Color(newColor));
+        return newColor;
     }
 
     private static float calcDistance(final int currentColor, final int desiredRed, final int desiredGreen, final int desiredBlue) {
@@ -97,7 +211,8 @@ public class JourneyMapToXaero {
         if (dimension == -1) {
             HashMap<String, HashSet<File>> allCaveFiles = new HashMap<>();   // region file -> all cave file locations
             for (File folder : Objects.requireNonNull(folderIn.toFile().listFiles())) {
-                Arrays.stream(folder.listFiles()).parallel().forEach(file -> {
+                Arrays.stream(folder.listFiles())//.parallel()
+                        .forEach(file -> {
                     String layer = file.getParentFile().getName();  // 0, 1, 2, day, night
                     String region = file.getName();  // 1,1.png
                     if (caveLayers.contains(layer)) {  // if this folder is not day, night, or something weird
@@ -170,15 +285,31 @@ public class JourneyMapToXaero {
 
     private static class IronBlock {
 
-        private final int journeymapColor;
 
         private final boolean nether;
 
-        private final int colourType =0; // 3 for no complexity
+        private final int colourType; // -1 will destruct whole chunks, 0 good
+        private final int color;
+
+        private final int state;
+        private final int biome;
+
+
+
+        public IronBlock(final int blockState, final int colorType,final int color, final int biome) {
+            this.colourType = colorType;
+            this.state = blockState;
+            this.color = color;
+            this.biome = biome;
+            nether = false;
+        }
 
         public IronBlock(final int jmColor, boolean nether) {
-            this.journeymapColor = jmColor;
             this.nether = nether;
+            this.state = COLOR_TO_STATE.get(getClosestColor(jmColor));
+            colourType = 0;
+            this.color = 0xFF000000;
+            biome = 1;// plains
         }
 
 
@@ -218,11 +349,11 @@ public class JourneyMapToXaero {
         }
 
         public int getCustomColour() {
-            return 0xFF000000;
+            return color;
         }
 
         public int getBiome() {
-            return 1; // plains
+            return biome;
         }
     }
 
@@ -254,7 +385,7 @@ public class JourneyMapToXaero {
                                     for (int z = 0; z < 16; ++z) {
                                         int relX = 64 * o + 16 * i + x;
                                         int relZ = 64 * p + 16 * j + z;
-                                        this.savePixel(new IronBlock(image.getRGB(relX, relZ), nether), out);
+                                        savePixel(new IronBlock(image.getRGB(relX, relZ), nether), out);
                                     }
                                 }
                                 out.write(0); // some version thing
@@ -273,10 +404,11 @@ public class JourneyMapToXaero {
         }
     }
 
-    private void savePixel(final IronBlock pixel, final DataOutputStream out) throws IOException {
+    private static void savePixel(final IronBlock pixel, final DataOutputStream out) throws IOException {
         final int parameters = pixel.getParameters();
         out.writeInt(parameters);
-        if (pixel.isNotGrass()) {
+        if (! pixel.isGrass()) {
+//            System.out.println("Writing blockstate" + pixel.getState());
             out.writeInt(pixel.getState());
         }
 
