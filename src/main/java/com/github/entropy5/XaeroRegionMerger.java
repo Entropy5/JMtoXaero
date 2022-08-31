cbe6dd4b1050fa208e6fbe4dd74e5db31730c22b
Entropy5 <44469915+Entropy5@users.noreply.github.com>
2022-08-31 08:06:02 +0200

    reading and interleaving works, writing has a bug still

--- src/main/java/com/github/entropy5/XaeroRegionMerger.java
+++ src/main/java/com/github/entropy5/XaeroRegionMerger.java
@@ -46,15 +46,16 @@ public class XaeroRegionMerger {
             File fileIn1 = inp1.resolve(rName).toFile();
             File fileIn2 = inp2.resolve(rName).toFile();
             File fileOut = outp.resolve(rName).toFile();
-            DataInputStream in = null;
+            DataInputStream in1 = null;
             DataInputStream in2 = null;
             DataOutputStream out = null;
-            int saveVersion = -1;
+            int saveVersionA = -1;
+            int saveVersionB = -1;
             try {
                 try {
                     ZipInputStream zipIn1 = new ZipInputStream(new BufferedInputStream(Files.newInputStream(fileIn1.toPath()), 2048));
                     ZipInputStream zipIn2 = new ZipInputStream(new BufferedInputStream(Files.newInputStream(fileIn2.toPath()), 2048));
-                    in = new DataInputStream(zipIn1);
+                    in1 = new DataInputStream(zipIn1);
                     in2 = new DataInputStream(zipIn2);
                     zipIn1.getNextEntry();
                     zipIn2.getNextEntry();
@@ -63,78 +64,130 @@ public class XaeroRegionMerger {
                     out = new DataOutputStream(zipOut);
                     final ZipEntry e = new ZipEntry("region.xaero");
                     zipOut.putNextEntry(e);
-                    out.write(255);  // mimicking logic from Xaero format
-                    out.writeInt(4);
 
-
-                    int firstByte = in.read();
-                    System.out.println("firstbyte: " + firstByte);
-                    if (firstByte == 255) {
-                        saveVersion = in.readInt();
-                        System.out.println("version: " + saveVersion);
-                        if (4 < saveVersion) {
-                            zipIn1.closeEntry();
-                            in.close();
+                    int firstByteA = in1.read();
+                    int firstByteB = in2.read();
+                    System.out.println("firstbyte: " + firstByteA + ", " + firstByteB);
+                    if (firstByteA == 255) {
+                        out.write(255);
+                        saveVersionA = in1.readInt();
+                        saveVersionB = in2.readInt();
+                        System.out.println("version: " + saveVersionA + ", " + saveVersionB);
+                        if (saveVersionA == 4 && saveVersionB == 4) {
+                            out.writeInt(4);
+                        } else {
                             throw new RuntimeException(rName + " made using newer xaero version");
                         }
-                        firstByte = -1;
+                        firstByteA = -1;
+                        firstByteB = -1;
                     }
-                    int p;
-
+                    Process tileProc = Process.BOTH;
+                    int tileChunkCoordsA = 0;
+                    int tileChunkCoordsB = 0;
                     while (true) {  // Keeps reading TileChunks (max 8x8) until done
-                        int chunkCoords = firstByte == -1 ? in.read() : firstByte;
-                        System.out.println("ChunkCoords: " + chunkCoords);
-                        if (chunkCoords == -1) {  // -1 as chunk coord means its over
+                        if (tileProc == Process.A || tileProc == Process.BOTH) {
+                            if (firstByteA == -1) {
+                                tileChunkCoordsA = in1.read();
+                                out.write(tileChunkCoordsA);
+                            } else {
+                                tileChunkCoordsA = firstByteA;
+                            }
+                        }
+                        if (tileProc == Process.B || tileProc == Process.BOTH) {
+                            if (firstByteB == -1) {
+                                tileChunkCoordsB = in2.read();
+                                if (tileProc == Process.B) {  // only case where it wasn't written already
+                                    out.write(tileChunkCoordsB);
+                                }
+                            } else {
+                                tileChunkCoordsB = firstByteB;
+                            }
+                        }
+                        System.out.println("ChunkCoords: " + tileChunkCoordsA + ", " + tileChunkCoordsB);
+                        if (tileChunkCoordsA == -1 && tileChunkCoordsB == -1) {  // -1 as chunk coord means its over
                             System.out.println("closing");
                             zipIn1.closeEntry();
+                            zipIn2.closeEntry();
+                            zipOut.closeEntry();
                             break;
                         }
-
-                        firstByte = -1;
-                        int o = chunkCoords >> 4;
-                        p = chunkCoords & 15;
-                        System.out.println("Chunk: " + o + "," + p);
+                        tileProc = tileChunkCoordsA < tileChunkCoordsB ? Process.A : (tileChunkCoordsA == tileChunkCoordsB ? Process.BOTH : Process.B);
+                        System.out.println(tileProc);
+                        firstByteA = -1;
+                        firstByteB = -1;
+                        System.out.println("Chunk: " + (tileChunkCoordsA >> 4) + "," + (tileChunkCoordsA & 15));
+                        System.out.println("Chunk: " + (tileChunkCoordsB >> 4) + "," + (tileChunkCoordsB & 15));
 
                         for (int i = 0; i < 4; ++i) {
                             for (int j = 0; j < 4; ++j) {
-                                Integer nextTile = in.readInt();
-                                System.out.println("parameters: " + nextTile);
-                                if (nextTile != -1) {  // Skip empty chunk
-                                    for (int x = 0; x < 16; ++x) {
-                                        for (int z = 0; z < 16; ++z) {
-                                            passPixel(nextTile, in, out, saveVersion, true);
-                                            nextTile = null;
-                                        }
-                                    }
-                                    if (saveVersion == 4) {
-                                        int worldInterVersion = in.read();
-                                    }
+                                if (tileProc == Process.A) {
+                                    Integer nextTile = in1.readInt();
+                                    out.writeInt(nextTile);
+                                    passChunk(nextTile, in1, out, true);
+                                } else if (tileProc == Process.B) {
+                                    Integer nextTile = in2.readInt();
+                                    out.writeInt(nextTile);
+                                    passChunk(nextTile, in2, out, true);
                                 } else {
-                                    System.out.println("Empty chunk " + o + "," + p + "," + i + "," + j);
+                                    Integer nextTileA = in1.readInt();
+                                    Integer nextTileB = in2.readInt();
+                                    out.writeInt(nextTileA);
+                                    passChunk(nextTileA, nextTileB, in1, in2, out);  // Deep merge
                                 }
                             }
                         }
                     }
                 } finally {
-                    if (in != null) {
-                        in.close();
+                    if (in1 != null) {
+                        in1.close();
+                    }
+                    if (in2 != null) {
+                        in2.close();
+                    }
+                    if (out != null) {
+                        out.close();
                     }
                 }
-
             } catch (Exception e) {
                 e.printStackTrace();
             }
-
         }
     }
 
-    private static void passPixel(Integer next, DataInputStream in, DataOutputStream out, int saveVersion, boolean write) throws IOException {
+    private static void passChunk(Integer nextTile, DataInputStream in, DataOutputStream out, boolean write) throws IOException {
+        if (nextTile != -1) {  // Skip empty chunk
+            for (int x = 0; x < 16; ++x) {
+                for (int z = 0; z < 16; ++z) {
+                    passPixel(nextTile, in, out, write);
+                    nextTile = null;
+                }
+            }
+            int worldInterVersion = in.read();
+            if (write) {
+                out.write(worldInterVersion);
+            }
+        }
+    }
+
+    private static void passChunk(Integer nextTileA, Integer nextTileB, DataInputStream in1, DataInputStream in2, DataOutputStream out) throws IOException {
+        if (nextTileA != -1) {  // A gets priority
+            passChunk(nextTileA, in1, out, true);
+            if (nextTileB != -1) {
+                passChunk(nextTileB, in2, out, false);
+            }
+        } else if (nextTileB != -1) {
+            passChunk(nextTileB, in2, out, true);
+        }
+    }
+
+
+    private static void passPixel(Integer next, DataInputStream in, DataOutputStream out, boolean write) throws IOException {
         int parametres;
         if (next != null) {
             parametres = next;
         } else {
             parametres = in.readInt();
-            System.out.println("special parameters: " + parametres);
+//            System.out.println("special parameters: " + parametres);
             if (write) {
                 out.writeInt(parametres);
             }
@@ -142,7 +195,7 @@ public class XaeroRegionMerger {
 
         if ((parametres & 1) != 0) {
             int state = in.readInt();
-            System.out.println("counter: " + counter + " - state: " + state);
+            System.out.println("counter: " + counter + " - state: " + state + " - write: " + write);
             counter++;
             if (write) {
                 out.writeInt(state);
@@ -151,7 +204,7 @@ public class XaeroRegionMerger {
 
         if ((parametres & 64) != 0) {
             int height = in.read();
-            System.out.println("height: " + height);
+//            System.out.println("height: " + height);
             if (write) {
                 out.write(height);
             }
@@ -159,25 +212,28 @@ public class XaeroRegionMerger {
 
         if ((parametres & 16777216) != 0) {
             int topHeight = in.read();
+            if (write) {
+                out.write(topHeight);
+            }
         }
 
         int savedColourType;
         int biomeKey;
         if ((parametres & 2) != 0) {
             savedColourType = in.read();
-            System.out.println("savedcolortype: " + savedColourType);
+//            System.out.println("savedcolortype: " + savedColourType);
             if (write) {
                 out.write(savedColourType);
             }
 
             for(biomeKey = 0; biomeKey < savedColourType; ++biomeKey) {
-                passOverlay(in, out, saveVersion, write);
+                passOverlay(in, out, write);
             }
         }
         savedColourType = parametres >> 2 & 3;
         if (savedColourType == 3) {
             int customColour = in.readInt();
-            System.out.println("customc: " + customColour);
+//            System.out.println("customc: " + customColour);
             if (write) {
                 out.writeInt(customColour);
             }
@@ -189,7 +245,7 @@ public class XaeroRegionMerger {
             if (write) {
                 out.write(biomeByte);
             }
-            if (saveVersion >= 3 && biomeByte >= 255) {
+            if (biomeByte >= 255) {
                 biomeKey = in.readInt();
                 if (write) {
                     out.writeInt(biomeKey);
@@ -199,8 +255,11 @@ public class XaeroRegionMerger {
     }
 
 
-    private static void passOverlay(DataInputStream in, DataOutputStream out, int saveVersion, boolean write) throws IOException {
+    private static void passOverlay(DataInputStream in, DataOutputStream out, boolean write) throws IOException {
         int parametres = in.readInt();
+        if (write) {
+            out.writeInt(parametres);
+        }
         int state;
         if ((parametres & 1) != 0) {
             state = in.readInt();
@@ -209,12 +268,6 @@ public class XaeroRegionMerger {
             }
         }
         int opacity = 1;
-        if (saveVersion < 1 && (parametres & 2) != 0) {
-            int something = in.readInt();
-            if (write) {
-                out.writeInt(something);
-            }
-        }
         byte savedColourType = (byte)(parametres >> 8 & 3);
         if (savedColourType == 2 || (parametres & 4) != 0) {
             int biomeBuffer = in.readInt();
@@ -247,5 +300,11 @@ public class XaeroRegionMerger {
         Arrays.stream(Objects.requireNonNull(folder.toFile().listFiles()))
                 .forEach(fileIn -> nameSet.add(fileIn.getName()));
         return nameSet;
+    }
+
+    enum Process {
+        A,
+        B,
+        BOTH
     }
 }
